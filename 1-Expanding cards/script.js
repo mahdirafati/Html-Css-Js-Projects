72785dd546de935e4f69a3a5c53b10151d8fc00b
Mahdi Rafati <mahdirafati680@gmail.com>
2022-03-06 15:25:17 +0330

    Comments added

--- 1-Expanding cards/script.js
+++ 1-Expanding cards/script.js
@@ -4,18 +4,21 @@ const panels = document.querySelectorAll('.panel');
 //EventListeners
 eventListeners();
 function eventListeners(){
+    //add listener on every panel element
     panels.forEach(element => {
         element.addEventListener('click', activePicture)
     })
 }
 //Functions
+
+//To add active class for clicked element
 function activePicture(){
     //remove .active from others
     removeActive();
     //add .active class
     this.classList.add('active');
 }
-
+//To remove active class from last element
 function removeActive(){
     panels.forEach(element =>{
         if(element.classList.contains('active')){
